ddbace04979184942068ba5425434e8db3904f32
Josh Vocal <joshvocal@gmail.com>
2017-09-04 09:58:25 -0700

    Added Butterknife to MainActivity

--- app/src/main/java/me/joshvocal/translinkme_app/activities/MainActivity.java
+++ app/src/main/java/me/joshvocal/translinkme_app/activities/MainActivity.java
@@ -5,43 +5,62 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import me.joshvocal.translinkme_app.R;
+import me.joshvocal.translinkme_app.fragments.FavouritesFragment;
+import me.joshvocal.translinkme_app.fragments.LocationFragment;
+import me.joshvocal.translinkme_app.fragments.SearchFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_location:
-                    mTextMessage.setText(R.string.title_location);
-                    return true;
-                case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
-                    return true;
-                case R.id.navigation_favourite:
-                    mTextMessage.setText(R.string.title_favourites);
-                    return true;
-            }
-            return false;
-        }
-
-    };
+    @BindView(R.id.navigation)
+    BottomNavigationView mBottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(this);
     }
 
+    private void switchToSearchFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, new SearchFragment())
+                .commit();
+    }
+
+    private void switchToFavouritesFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, new FavouritesFragment())
+                .commit();
+    }
+
+    private void switchToLocationFragment() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content, new LocationFragment())
+                .commit();
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_location:
+                return true;
+            case R.id.navigation_search:
+                return true;
+            case R.id.navigation_favourite:
+                return true;
+        }
+       
+        return false;
+    }
 }
---
+++ app/src/main/java/me/joshvocal/translinkme_app/fragments/LocationFragment.java
@@ -0,0 +1,24 @@
+package me.joshvocal.translinkme_app.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.joshvocal.translinkme_app.R;
+
+public class LocationFragment extends Fragment {
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_location, container, false);
+        return rootView;
+    }
+}
