6b3fa201ab437bca5582d1615946f45f51d3dab8
jvocal <jvocal@sc-2300-46.labs.surrey.sfu.ca>
2017-09-06 13:09:54 -0700

    Added About AlertDialog

--- app/src/main/java/me/joshvocal/translinkme_app/activity/MainActivity.java
+++ app/src/main/java/me/joshvocal/translinkme_app/activity/MainActivity.java
@@ -1,11 +1,16 @@
 package me.joshvocal.translinkme_app.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -83,6 +88,34 @@ public class MainActivity extends AppCompatActivity implements
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.menu_main_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+                builder.setTitle("TransLinkMe v0.0.1")
+                        .setMessage("TransLinkMe is an open-source app developed by Josh Vocal.\n\n" +
+                                "As a frequent bus user, this makes it easier to check when the bus comes for my usual bus stops." +
+                        "Feel free to rate this app whatever it deserves.")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                // Do nothing.
+                            }
+                        });
+
+                AlertDialog dialog = builder.show();
+
+                dialog.show();
+
+                return true;
+        }
 
         return true;
     }
