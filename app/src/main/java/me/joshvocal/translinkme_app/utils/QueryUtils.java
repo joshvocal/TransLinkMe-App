1d1fc021ac43ca323c455b4b473a4b7a428a608a
Josh Vocal <joshvocal@gmail.com>
2017-09-07 07:45:13 -0700

    Removed unused function

--- app/src/main/java/me/joshvocal/translinkme_app/utils/QueryUtils.java
+++ app/src/main/java/me/joshvocal/translinkme_app/utils/QueryUtils.java
@@ -35,49 +35,6 @@ public class QueryUtils {
         return url;
     }
 
-    public static String makeHttpRequest(URL url) throws IOException {
-        String jsonResponse = "";
-
-        // If the Url is null, then return early.
-        if (url == null) {
-            return jsonResponse;
-        }
-
-        HttpURLConnection urlConnection = null;
-        InputStream inputStream = null;
-
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setReadTimeout(10000);
-            urlConnection.setConnectTimeout(15000);
-            urlConnection.setRequestMethod("GET");
-            urlConnection.connect();
-
-            // If the request was successful (response code 200),
-            // the read the input stream and parse the response.
-            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                inputStream = urlConnection.getInputStream();
-                jsonResponse = readFromStream(inputStream);
-            } else {
-                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
-            }
-
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem retrieving the JSON results.", e);
-        } finally {
-
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        }
-
-        return jsonResponse;
-    }
-
     public static String makeHttpRequestForJson(URL url) throws IOException {
         String jsonResponse = "";
 
